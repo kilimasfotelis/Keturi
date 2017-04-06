be86dbed57c4126c28231b9c537bf491a277a829
kilimas fotelis <kilimasfotelis@gmail.com>
2017-04-06 13:46:53 +0300

    1

---
+++ Keturi/Controllers/NumberController.cs
@@ -0,0 +1,48 @@
+﻿using Keturi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Keturi.Controllers
+{
+    public class NumberController : Controller
+    {
+        // GET: Number
+        public ActionResult Index()
+        {
+            Number n = new Number();
+            n.Generate();
+            n.createNotes(new List<string>());
+            string answer = n.Answer;
+            Session["answer"] = answer;
+            Session["n"] = n;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string guess)
+        {
+
+            Number n = (Number)Session["n"];
+            if (n.compareValues(guess, n.Answer))
+            {
+                return RedirectToAction("Win");
+            }
+            else
+            {
+                ViewBag.list = n.Notes;
+                return View();
+            }
+
+        }
+
+        public ActionResult Win()
+        {
+            Number n = (Number)Session["n"];
+            ViewBag.count = n.Notes.Count();
+            return View();
+        }
+    }
+}
---
+++ Keturi/Models/Number.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Keturi.Models
+{
+    public class Number
+    {
+        public string Answer { get; set; }
+        public Number() { }
+        public string FinalAnswer { get; set; }
+        public List<string> Notes { get; set; }
+        public void createNotes(List<string> notes)
+        {
+            this.Notes = notes;
+        }
+
+        //Metodas skaiciui sugeneruoti
+        public void Generate()
+        {
+            //Hashsetas supildomas skaitmenimis
+            //
+            int number;
+            HashSet<int> Answer = new HashSet<int>();
+            while (Answer.Count != 4)
+            {
+                Random random = new Random();
+                number = random.Next(0, 9999);
+                int digit = number % 10;
+                Answer.Add(digit);
+            }
+
+            //Hashsetas paverciamas string'u
+            // 
+            string answer = "";
+            foreach (var item in Answer)
+            {
+                answer += item.ToString();
+            }
+            this.Answer = answer;
+        }
+
+        // tikrinama ar ivestas skaicius atitinka sugeneruota
+        // jeigu neatitinka tuomet deda ji i sarasa
+        public Boolean compareValues(string guess, string answer)
+        {
+            bool ok = false;
+            if (guess == answer)
+            {
+                ok = true;
+            }
+            else
+            {
+                int semiCorrect = 0; int correct = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    if (answer.Contains(guess[i]))
+                    {
+                        semiCorrect++;
+                    }
+                    if (answer[i] == guess[i])
+                    {
+                        correct++;
+                    }
+                }
+                Notes.Insert(0, string.Format("Jūsų spėjimas:{0}, Atspėta skaitmenų: {1}, Iš jų savo vietoje: {2}", guess, semiCorrect.ToString(), correct.ToString()));
+            }
+            return ok;
+        }
+    }
+}
