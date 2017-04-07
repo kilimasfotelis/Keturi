54967e75ca22ed3bd955d5c1ecc23792d37d174e
kilimas fotelis <kilimasfotelis@gmail.com>
2017-04-07 15:38:28 +0300

    highscore

---
+++ Keturi/Controllers/HighscoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Keturi.Models;
+
+namespace Keturi.Controllers
+{
+    public class HighscoresController : Controller
+    {
+        private HighscoreDBContext db = new HighscoreDBContext();
+
+        // GET: Highscores
+        public ActionResult Index()
+        {
+            return View(db.Highscores.ToList());
+        }
+
+        // GET: Highscores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscores.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // GET: Highscores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Highscores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nickname,Score")] Highscore highscore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Highscores.Add(highscore);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(highscore);
+        }
+
+        // GET: Highscores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscores.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // POST: Highscores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nickname,Score")] Highscore highscore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(highscore).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(highscore);
+        }
+
+        // GET: Highscores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscores.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // POST: Highscores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Highscore highscore = db.Highscores.Find(id);
+            db.Highscores.Remove(highscore);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
